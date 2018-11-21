044688ad0304f93ed861c23a2cbe5a681abfa83e
Esteban Rao <estebanrao@gmail.com>
2018-11-21 18:45:07 -0300

    Add server starter code

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo
+        .save()
+        .then((doc) => {
+            res.send(doc);
+        }, (err) => {
+            res.status(400).send(err);
+        });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
