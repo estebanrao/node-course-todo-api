ec9ce65813b6fa500563ee5555471024c94ec688
Esteban Rao <estebanrao@gmail.com>
2018-11-22 17:48:12 -0300

    Adds PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -65,6 +66,39 @@ app.delete('/todos/:id', (req, res) => {
 
     Todo
         .findByIdAndRemove(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({ todo });
+        })
+        .catch((err) => {
+            res.status(400).send();
+        })
+})
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo
+        .findByIdAndUpdate(id, {
+            $set: body
+        }, {
+            new: true
+        })
         .then((todo) => {
             if (!todo) {
                 return res.status(404).send();
