0c6f98740232f1d7d4a0e48961d18560ddeb814c
Esteban Rao <estebanrao@gmail.com>
2018-11-22 14:46:45 -0300

    Adds DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
         .findById(id)
         .then((todo) => {
             if (!todo) {
-                res.status(404).send();
+                return res.status(404).send();
             }
 
             res.send({ todo });
@@ -55,6 +55,27 @@ app.get('/todos/:id', (req, res) => {
             res.status(400).send();
         })
 });
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo
+        .findByIdAndRemove(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({ todo });
+        })
+        .catch((err) => {
+            res.status(400).send();
+        })
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
