f79ef84e220008e1fd60e864123feedd5ef0e67e
Esteban Rao <estebanrao@gmail.com>
2018-11-22 13:32:13 -0300

    Fix port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 let app = express();
-const port = process.env.POST || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
