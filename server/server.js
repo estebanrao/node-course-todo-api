c819bb2a92b3b39896eab8b11953e7270f0bfe00
Esteban Rao <estebanrao@gmail.com>
2018-11-23 13:40:29 -0300

    Adds POST /users route, token method and editor configs

--- server/server.js
+++ server/server.js
@@ -16,26 +16,28 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
     });
 
-    todo
-        .save()
-        .then((doc) => {
+    todo.save().then(
+        doc => {
             res.send(doc);
-        }, (err) => {
+        },
+        err => {
             res.status(400).send(err);
-        });
+        }
+    );
 });
 
 app.get('/todos', (req, res) => {
-    Todo
-        .find()
-        .then((todos) => {
+    Todo.find().then(
+        todos => {
             res.send({ todos });
-        }, (err) => {
+        },
+        err => {
             res.status(400).send(err);
-        })
+        }
+    );
 });
 
 app.get('/todos/:id', (req, res) => {
@@ -45,18 +47,17 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
 
-    Todo
-        .findById(id)
-        .then((todo) => {
+    Todo.findById(id)
+        .then(todo => {
             if (!todo) {
                 return res.status(404).send();
             }
 
             res.send({ todo });
         })
-        .catch((err) => {
+        .catch(err => {
             res.status(400).send();
-        })
+        });
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -66,19 +67,18 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
 
-    Todo
-        .findByIdAndRemove(id)
-        .then((todo) => {
+    Todo.findByIdAndRemove(id)
+        .then(todo => {
             if (!todo) {
                 return res.status(404).send();
             }
 
             res.send({ todo });
         })
-        .catch((err) => {
+        .catch(err => {
             res.status(400).send();
-        })
-})
+        });
+});
 
 app.patch('/todos/:id', (req, res) => {
     const id = req.params.id;
@@ -95,28 +95,47 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo
-        .findByIdAndUpdate(id, {
-            $set: body
-        }, {
-            new: true
-        })
-        .then((todo) => {
+    Todo.findByIdAndUpdate(
+        id,
+        {
+            $set: body,
+        },
+        {
+            new: true,
+        }
+    )
+        .then(todo => {
             if (!todo) {
                 return res.status(404).send();
             }
 
             res.send({ todo });
         })
-        .catch((err) => {
+        .catch(err => {
             res.status(400).send();
+        });
+});
+
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
         })
-})
+        .then(token => {
+            res.header('x-auth', token).send(user);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
 module.exports = {
-    app
+    app,
 };
