444930f8c2ca1b47a1b06c718873bb16ef4cdea2
Esteban Rao <estebanrao@gmail.com>
2018-11-26 17:18:13 -0300

    Adds DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function() {
     return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    })
+}
+
 UserSchema.statics.findByToken = function(token) {
     const User = this;
     let decoded;
